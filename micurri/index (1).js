119e53a795dd688ec9680e5b2b59d2972ed33c1c
alejandrag2007 <162740259+alejandrag2007@users.noreply.github.com>
2024-03-12 21:36:34 -0600

    eso todo bien

---
+++ micurri/index (1).js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector(".toggle")
+const navMenu = document.querySelector(".nav-menu")
+toggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("nav-menvi");
+});
